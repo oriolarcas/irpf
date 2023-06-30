55cce1c0920a47686acae26a370fa9d1ff82e118
Oriol Arcas <oriol.arcas@gmail.com>
2023-06-30 23:42:31 +0000

    Quota SS

--- irpf/src/Irpf.ts
+++ irpf/src/Irpf.ts
@@ -23,12 +23,20 @@ export enum ComunitatAutònoma {
     CastellaILleó,
     CastellaLaManxa,
     Catalunya,
-    ComunitatValenciana,
+    PaísValencià,
     Extremadura,
     Galícia,
     LaRioja,
     Madrid,
     Múrcia,
+}
+
+export interface IrpfParameters {
+    salariBrut: number;
+    edat: number;
+    categoriaProfessional: CategoriaProfessional;
+    comunitatAutònoma: ComunitatAutònoma;
+    movilitatGeogràfica: boolean;
 }
 
 interface IrpfResult {
@@ -38,12 +46,50 @@ interface IrpfResult {
     salariNet: number;
 }
 
-export function calculateIrpf(
-    salariBrut: number,
-    edat: number,
-    categoriaProfessional: CategoriaProfessional,
-    comunitatAutònoma: ComunitatAutònoma,
-    movilitatGeogràfica: boolean,
-): IrpfResult {
-    return {valid: true, quotaSS: 0.0, tipusRetenció: 0.0, salariNet: 0.0};
+function límitsCotitzacióPerCategoriaProfessional(categoriaProfessional: CategoriaProfessional): [min: number, max: number] {
+    switch (categoriaProfessional) {
+        case CategoriaProfessional.EnginyersILlicenciats:
+            return [1629.30, 4139.40];
+        case CategoriaProfessional.EnginyersTecnicsPèritsIAjudantsTitulats:
+            return [1351.20, 4070.10];
+        case CategoriaProfessional.CapsAdministratiusIDeTaller:
+            return [1175.40, 4139.40];
+        case CategoriaProfessional.AjudantsNoTitulats:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.OficialsAdministratius:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.Subalterns:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.AuxiliarsAdministratius:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.OficialsDePrimeraISegona:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.OficialsDeTerceraIEspecialistes:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.Peons:
+            return [1166.70, 4139.40];
+        case CategoriaProfessional.TreballadorsMenors18Anys:
+            return [1166.70, 4139.40];
+    }
+}
+
+const PercentatgeCotitzacióContingènciesComunes = 0.047;
+const PercentatgeCotitzacióFormació = 0.001;
+const PercentatgeCotitzacióAtur = 0.0155;
+const PercentatgeCotitzacióSS = PercentatgeCotitzacióContingènciesComunes + PercentatgeCotitzacióFormació + PercentatgeCotitzacióAtur;
+
+export function calculateIrpf({
+    salariBrut,
+    edat,
+    categoriaProfessional,
+    comunitatAutònoma,
+    movilitatGeogràfica,
+}: IrpfParameters): IrpfResult {
+    const salariBrutMensual = salariBrut / 12;
+
+    const [minCotitzacióSS, maxCotitzacióSS] = límitsCotitzacióPerCategoriaProfessional(categoriaProfessional);
+    const CotitzacióSSMensual = Math.min(Math.max(salariBrutMensual, minCotitzacióSS), maxCotitzacióSS) * PercentatgeCotitzacióSS;
+    const CotitzacióSSAnual = CotitzacióSSMensual * 12;
+
+    return { valid: true, quotaSS: CotitzacióSSAnual, tipusRetenció: 0.0, salariNet: 0.0 };
 }
