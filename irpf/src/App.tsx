4cfae976792600b34b782afe368b4a6ca4700d49
Oriol Arcas <oriol.arcas@gmail.com>
2023-06-30 23:42:13 +0000

    Use state

--- irpf/src/App.tsx
+++ irpf/src/App.tsx
@@ -1,36 +1,36 @@
 import Sidebar from './Sidebar'
+import { IrpfParameters, calculateIrpf } from './Irpf';
 
 import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
-import { CalculatorFill, CurrencyEuro, Person, GeoAltFill, Tools } from 'react-bootstrap-icons';
+class App extends React.Component {
+  inputEvent = (params: IrpfParameters) => {
+    console.log(params);
 
-const App: React.FC = () => {
-  const calculate = (event: any) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    // let calculator = calculateIrpf(form.);
+    const irpf = calculateIrpf(params);
+    console.log(irpf);
+  };
+
+  render() {
+    return (
+      <Container fluid className='h-100'>
+        <Row className='h-100'>
+          <Sidebar onInput={this.inputEvent} />
+
+          <Col fluid className="p-5">
+            <h1 className="header">
+              Welcome To React-Bootstrap TypeScript Example
+            </h1>
+            <h2>Buttons</h2>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
-
-  return (
-    <Container fluid className='h-100'>
-      <Row className='h-100'>
-        <Sidebar />
-
-        <Col fluid className="p-5">
-          <h1 className="header">
-            Welcome To React-Bootstrap TypeScript Example
-          </h1>
-          <h2>Buttons</h2>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+}
 
 export default App;
--- irpf/src/Sidebar.tsx
+++ irpf/src/Sidebar.tsx
@@ -1,6 +1,7 @@
-import { calculateIrpf, CategoriaProfessional, ComunitatAutònoma } from './Irpf';
+import { IrpfParameters, CategoriaProfessional, ComunitatAutònoma } from './Irpf';
 
 import React from 'react';
+import { useEffect } from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -8,12 +9,19 @@ import Button from 'react-bootstrap/Button';
 
 import { CalculatorFill, CurrencyEuro, Person, GeoAltFill, Tools } from 'react-bootstrap-icons';
 
-class Sidebar extends React.Component {
-    FormSalariBrut: any;
-    FormEdat: any;
-    FormCategoriaProfessional: any;
-    FormComunitatAutònoma: any;
-    FormMovilitatGeogràfica: any;
+enum IrpfFormFields {
+    SalariBrut = 'SalariBrut',
+    Edat = 'Edat',
+    CategoriaProfessional = 'CategoriaProfessional',
+    ComunitatAutònoma = 'ComunitatAutònoma',
+    MovilitatGeogràfica = 'MovilitatGeogràfica',
+}
+
+class Sidebar extends React.Component<{onInput?: (params: IrpfParameters) => void}> {
+    state = {
+        values: new Map<string, any>()
+    };
+    FormTextRefs = new Map<string, any>();
 
     SelectToCategoriaProfessional = new Map<string, CategoriaProfessional>([
         ['A', CategoriaProfessional.EnginyersILlicenciats],
@@ -40,31 +48,59 @@ class Sidebar extends React.Component {
         ['7', ComunitatAutònoma.CastellaILleó],
         ['8', ComunitatAutònoma.CastellaLaManxa],
         ['9', ComunitatAutònoma.Catalunya],
-        ['10', ComunitatAutònoma.ComunitatValenciana],
+        ['10', ComunitatAutònoma.PaísValencià],
         ['11', ComunitatAutònoma.Extremadura],
         ['12', ComunitatAutònoma.Galícia],
         ['13', ComunitatAutònoma.LaRioja],
         ['14', ComunitatAutònoma.Madrid],
         ['15', ComunitatAutònoma.Múrcia],
-    ])
+    ]);
 
     constructor(props: any) {
         super(props);
-        this.FormSalariBrut = React.createRef();
-        this.FormEdat = React.createRef();
-        this.FormCategoriaProfessional = React.createRef();
-        this.FormComunitatAutònoma = React.createRef();
-        this.FormMovilitatGeogràfica = React.createRef();
+
+        let values = this.state.values;
+        for (const field in IrpfFormFields) {
+            values.set(field, "");
+            this.FormTextRefs.set(field, React.createRef());
+        }
+        // Default values
+        values.set(IrpfFormFields.CategoriaProfessional, this.SelectToCategoriaProfessional.keys().next().value);
+        values.set(IrpfFormFields.ComunitatAutònoma, this.SelectToComunitatAutònoma.keys().next().value);
+        values.set(IrpfFormFields.MovilitatGeogràfica, false);
+
+        this.setState({values});
     }
 
-    calculate(event: any) {
+    onChange = (event: any) => {
+        const element = event.target;
+
+        console.log(element);
+
+        let values = this.state.values;
+        switch (element.id) {
+            case IrpfFormFields.SalariBrut:
+            case IrpfFormFields.Edat:
+            case IrpfFormFields.CategoriaProfessional:
+            case IrpfFormFields.ComunitatAutònoma:
+                values.set(element.id, element.value);
+                break;
+            case IrpfFormFields.MovilitatGeogràfica:
+                values.set(element.id, element.checked);
+                break;
+        }
+        this.setState({values});
+    }
+
+    onInput = (event: any) => {
         event.preventDefault();
 
-        const salariBrut = parseInt(this.FormSalariBrut.current.value);
-        const edat = parseInt(this.FormEdat.current.value);
-        const categoriaProfessional = this.SelectToCategoriaProfessional.get(this.FormCategoriaProfessional.current.value);
-        const comunitatAutònoma = this.SelectToComunitatAutònoma.get(this.FormComunitatAutònoma.current.value);
-        const movilitatGeogràfica = this.FormMovilitatGeogràfica.current.checked;
+        const values = this.state.values;
+        const salariBrut = parseInt(values.get(IrpfFormFields.SalariBrut));
+        const edat = parseInt(values.get(IrpfFormFields.Edat));
+        const categoriaProfessional = this.SelectToCategoriaProfessional.get(values.get(IrpfFormFields.CategoriaProfessional));
+        const comunitatAutònoma = this.SelectToComunitatAutònoma.get(values.get(IrpfFormFields.ComunitatAutònoma));
+        const movilitatGeogràfica = values.get(IrpfFormFields.MovilitatGeogràfica);
 
         if (Number.isNaN(salariBrut)) {
             this.error('Salari brut invàlid');
@@ -76,104 +112,109 @@ class Sidebar extends React.Component {
             return;
         }
 
-        if (categoriaProfessional == undefined) {
+        if (categoriaProfessional === undefined) {
             this.error('Categoria professional invàlida');
             return;
         }
 
-        if (comunitatAutònoma == undefined) {
+        if (comunitatAutònoma === undefined) {
             this.error('Comunitat autònoma invàlida');
             return;
         }
 
-        const irpf = calculateIrpf(
+        const params: IrpfParameters = {
             salariBrut,
             edat,
             categoriaProfessional,
             comunitatAutònoma,
             movilitatGeogràfica,
-        );
+        };
 
-        console.log(irpf);
-    }
+        if (this.props.onInput) {
+            this.props.onInput(params);
+        }
+    };
 
     error(msg: string) {
         console.error(msg);
     }
-    
+
     render() {
         return (
             <Col md={3} xl={2} className='sidebar col-auto px-sm-2 px-0 bg-dark'>
-            <a href="#" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <CalculatorFill className="bi me-2" size={40} />
-                <span className="fs-4">IRPF</span>
-            </a>
-            <hr />
-            <Form onSubmit={this.calculate.bind(this)}>
-                <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item">
-                    <Form.Label htmlFor='salari-brut'>
-                    <CurrencyEuro size={20} color='white' />
-                    Salari brut anual
-                    </Form.Label>
-                    <Form.Control id="salari-brut" ref={this.FormSalariBrut} />
-                </li>
-                <li className="nav-item">
-                    <Form.Label htmlFor='edat'>
-                    <Person size={20} color='white' />
-                    Edat
-                    </Form.Label>
-                    <Form.Control id="edat" ref={this.FormEdat} />
-                </li>
-                <li className="nav-item">
-                    <Form.Label htmlFor='categoria-professional'>
-                    <Tools size={20} color='white' />
-                    Categoria professional
-                    </Form.Label>
-                    <Form.Select id='categoria-professional' aria-label="Categoria professional" ref={this.FormCategoriaProfessional}>
-                        <option value='A'>Enginyers i llicenciats</option>
-                        <option value='B'>Enginyers tècnics, pèrits i ajudants titulats</option>
-                        <option value='C'>Caps administratius i de taller</option>
-                        <option value='D'>Ajudants no titulats</option>
-                        <option value='E'>Oficials administratius</option>
-                        <option value='F'>Subalterns</option>
-                        <option value='G'>Auxiliars administratius</option>
-                        <option value='H'>Oficials de primera i segona</option>
-                        <option value='I'>Oficials de tercera i especialistes</option>
-                        <option value='J'>Peons</option>
-                        <option value='K'>Qualsevol treballador menor de 18 anys</option>
-                    </Form.Select>
-                </li>
-                <li className="nav-item">
-                    <Form.Label htmlFor='comunitat-autonoma'>
-                    <GeoAltFill size={20} color='white' />
-                    Comunitat autònoma
-                    </Form.Label>
-                    <Form.Select id='comunitat-autonoma' aria-label="Categoria professional" ref={this.FormComunitatAutònoma}>
-                        <option value='E'>Espanya</option>
-                        <option value='1'>Andalusia</option>
-                        <option value='2'>Aragó</option>
-                        <option value='3'>Astúries</option>
-                        <option value='4'>Balears</option>
-                        <option value='5'>Canaries</option>
-                        <option value='6'>Cantàbria</option>
-                        <option value='7'>Castella i Lleó</option>
-                        <option value='8'>Castella - la Manxa</option>
-                        <option value='9'>Catalunya</option>
-                        <option value='10'>Comunitat Valenciana</option>
-                        <option value='11'>Extremadura</option>
-                        <option value='12'>Galícia</option>
-                        <option value='13'>La Rioja</option>
-                        <option value='14'>Madrid</option>
-                        <option value='15'>Múrcia</option>
-                    </Form.Select>
-                    <Form.Check type='checkbox' id='movilitat-geografica' label='Movilitat geogràfica' className='mt-2' ref={this.FormMovilitatGeogràfica} />
-                </li>
-                <li className="nav-item">
-                    <Button type='submit' variant="primary" className='w-100 mt-3'>Calcula</Button>
-                </li>
-                </ul>
-            </Form>
+                <a href="#" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <CalculatorFill className="bi me-2" size={40} />
+                    <span className="fs-4">IRPF</span>
+                </a>
+                <hr />
+                <Form onSubmit={this.onInput}>
+                    <ul className="nav nav-pills flex-column mb-auto">
+                        <li className="nav-item">
+                            <Form.Label htmlFor={IrpfFormFields.SalariBrut}>
+                                <CurrencyEuro size={20} color='white' />
+                                Salari brut anual
+                            </Form.Label>
+                            <Form.Control id={IrpfFormFields.SalariBrut} onChange={this.onChange} />
+                            <Form.Text id="passwordHelpBlock" className='text-warning d-none' >
+                                El teu salari brut anual en €. Un número positiu, sense punts, comes, espais o altres símbols.
+                            </Form.Text>
+                        </li>
+                        <li className="nav-item">
+                            <Form.Label htmlFor={IrpfFormFields.Edat}>
+                                <Person size={20} color='white' />
+                                Edat
+                            </Form.Label>
+                            <Form.Control id={IrpfFormFields.Edat} onChange={this.onChange} />
+                        </li>
+                        <li className="nav-item">
+                            <Form.Label htmlFor={IrpfFormFields.CategoriaProfessional}>
+                                <Tools size={20} color='white' />
+                                Categoria professional
+                            </Form.Label>
+                            <Form.Select id={IrpfFormFields.CategoriaProfessional} aria-label="Categoria professional" onChange={this.onChange}>
+                                <option value='A'>Enginyers i llicenciats</option>
+                                <option value='B'>Enginyers tècnics, pèrits i ajudants titulats</option>
+                                <option value='C'>Caps administratius i de taller</option>
+                                <option value='D'>Ajudants no titulats</option>
+                                <option value='E'>Oficials administratius</option>
+                                <option value='F'>Subalterns</option>
+                                <option value='G'>Auxiliars administratius</option>
+                                <option value='H'>Oficials de primera i segona</option>
+                                <option value='I'>Oficials de tercera i especialistes</option>
+                                <option value='J'>Peons</option>
+                                <option value='K'>Qualsevol treballador menor de 18 anys</option>
+                            </Form.Select>
+                        </li>
+                        <li className="nav-item">
+                            <Form.Label htmlFor={IrpfFormFields.ComunitatAutònoma}>
+                                <GeoAltFill size={20} color='white' />
+                                Comunitat autònoma
+                            </Form.Label>
+                            <Form.Select id={IrpfFormFields.ComunitatAutònoma} aria-label="Comunitat Autònoma" onChange={this.onChange}>
+                                <option value='E'>Espanya</option>
+                                <option value='1'>Andalusia</option>
+                                <option value='2'>Aragó</option>
+                                <option value='3'>Astúries</option>
+                                <option value='4'>Balears</option>
+                                <option value='5'>Canaries</option>
+                                <option value='6'>Cantàbria</option>
+                                <option value='7'>Castella i Lleó</option>
+                                <option value='8'>Castella - la Manxa</option>
+                                <option value='9'>Catalunya</option>
+                                <option value='10'>Comunitat Valenciana</option>
+                                <option value='11'>Extremadura</option>
+                                <option value='12'>Galícia</option>
+                                <option value='13'>La Rioja</option>
+                                <option value='14'>Madrid</option>
+                                <option value='15'>Múrcia</option>
+                            </Form.Select>
+                            <Form.Check type='checkbox' id={IrpfFormFields.MovilitatGeogràfica} label='Movilitat geogràfica' className='mt-2' onChange={this.onChange} />
+                        </li>
+                        <li className="nav-item">
+                            <Button type='submit' variant="primary" className='w-100 mt-3'>Calcula</Button>
+                        </li>
+                    </ul>
+                </Form>
             </Col>
         );
     }
