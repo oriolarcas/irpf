959b1913fd8da6b358439258d35349ab929d3592
Oriol Arcas <oriol.arcas@gmail.com>
2023-07-07 17:47:13 +0000

    Mínims per CCAA

--- irpf/src/Dades.ts
+++ irpf/src/Dades.ts
@@ -18,7 +18,8 @@ export enum ComunitatAutònoma {
 }
 
 export enum GrauDiscapacitat {
-    CapOMenor33,
+    Cap,
+    Menor33,
     De33A65,
     Major65,
 }
@@ -31,8 +32,8 @@ export interface Dependent {
 
 export interface DadesComunitatAutònoma {
     getMínimContribuent(edat: number): number;
-    getMínimDescendent(exclusiva: boolean, edat: number, index: number): number;
-    getMínimAscendent(edat: number): number;
+    getMínimDescendent(exclusiva: boolean, edat: number, discapacitat: GrauDiscapacitat, index: number): number;
+    getMínimAscendent(edat: number, discapacitat: GrauDiscapacitat): number;
     getMínimDiscapacitat(dependent: Dependent): number;
 }
 
@@ -44,48 +45,61 @@ class DadesEspanya implements DadesComunitatAutònoma {
         return {base: 5550, major65: 6700, major75: 8100};
     }
 
+    protected getDadesMínimDescendent(): {menor3: number, descendents: number[]} {
+        return {menor3: 2800, descendents: [2400, 2700, 4000, 4500]};
+    }
+
+    protected getDadesMínimAscendent(): {major65: number, major75: number} {
+        return {major65: 1150, major75: 2550};
+    }
+
+    protected getDadesMínimDiscapacitat(): {major33: number, major65: number, assistència: number} {
+        return {major33: 3000, major65: 9000, assistència: 3000};
+    }
+
     // Public methods
 
     getMínimContribuent(edat: number): number {
+        // https://sede.agenciatributaria.gob.es/Sede/ca_es/ayuda/manuales-videos-folletos/manuales-ayuda-presentacion/irpf-2022/8-cumplimentacion-irpf/8_3-adecuacion-impuesto-circunstancias-personales-familiares/8_3_2-minimo-contribuyente.html
         const dadesMínimContribuent = this.getDadesMínimContribuent();
         return edat >= 75 ? dadesMínimContribuent.base : (edat >= 65 ? dadesMínimContribuent.major65 : dadesMínimContribuent.major75);
     }
 
-    getMínimDescendent(exclusiva: boolean, edat: number, index: number): number {
-        if (edat >= 25) {
+    getMínimDescendent(exclusiva: boolean, edat: number, discapacitat: GrauDiscapacitat, index: number): number {
+        // https://sede.agenciatributaria.gob.es/Sede/ca_es/ayuda/manuales-videos-folletos/manuales-ayuda-presentacion/irpf-2022/8-cumplimentacion-irpf/8_3-adecuacion-impuesto-circunstancias-personales-familiares/8_3_3-minimo-descendiente.html
+        if (edat >= 25 && discapacitat == GrauDiscapacitat.Cap) {
             return 0;
         }
         const coeficient = exclusiva ? 1 : 0.5;
-        switch (index) {
-            case 0:
-                return (edat < 3 ? 2800 : 2400) * coeficient;
-            case 1:
-                return 2700 * coeficient;
-            case 2:
-                return 4000 * coeficient;
-            default:
-                return 4500 * coeficient;
-        }
+        const dadesMínimDescendent = this.getDadesMínimDescendent();
+        const mínimDescendent = dadesMínimDescendent.descendents[Math.min(index, dadesMínimDescendent.descendents.length - 1)];
+        const mínimMenor3 = (edat < 3 ? dadesMínimDescendent.menor3 : 0);
+        return (mínimDescendent + mínimMenor3) * coeficient;
     }
 
-    getMínimAscendent(edat: number): number {
+    getMínimAscendent(edat: number, discapacitat: GrauDiscapacitat): number {
+        // https://sede.agenciatributaria.gob.es/Sede/ca_es/ayuda/manuales-videos-folletos/manuales-ayuda-presentacion/irpf-2022/8-cumplimentacion-irpf/8_3-adecuacion-impuesto-circunstancias-personales-familiares/8_3_4-minimo-ascendiente.html
+        const dadesMínimAscendent = this.getDadesMínimAscendent();
         if (edat >= 75) {
-            return 2250;
+            return dadesMínimAscendent.major75;
         }
-        if (edat >= 65) {
-            return 1150;
+        if (edat >= 65 || discapacitat != GrauDiscapacitat.Cap) {
+            return dadesMínimAscendent.major65;
         }
         return 0;
     }
 
     getMínimDiscapacitat(dependent: Dependent): number {
+        // https://sede.agenciatributaria.gob.es/Sede/ca_es/ayuda/manuales-videos-folletos/manuales-ayuda-presentacion/irpf-2022/8-cumplimentacion-irpf/8_3-adecuacion-impuesto-circunstancias-personales-familiares/8_3_5-minimo-discapacidad.html
+        const dadesMínimDiscapacitat = this.getDadesMínimDiscapacitat();
         switch (dependent.discapacitat) {
-            case GrauDiscapacitat.CapOMenor33:
+            case GrauDiscapacitat.Cap:
+            case GrauDiscapacitat.Menor33:
                 return 0;
             case GrauDiscapacitat.De33A65:
-                return 3000 + (dependent.assistència ? 3000 : 0);
+                return dadesMínimDiscapacitat.major33 + (dependent.assistència ? dadesMínimDiscapacitat.assistència : 0);
             case GrauDiscapacitat.Major65:
-                return 12000;
+                return dadesMínimDiscapacitat.major65 + (dependent.assistència ? dadesMínimDiscapacitat.assistència : 0);
         }
     }
 }
@@ -93,6 +107,14 @@ class DadesEspanya implements DadesComunitatAutònoma {
 class DadesAndalusia extends DadesEspanya {
     protected getDadesMínimContribuent() {
         return {base: 5790, major65: 6990, major75: 8450};
+    }
+
+    protected getDadesMínimDescendent() {
+        return {menor3: 2920, descendents: [2510, 2820, 4170, 4700]};
+    }
+
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3130, major65: 9390, assistència: 3130};
     }
 }
 
@@ -107,6 +129,17 @@ class DadesAstúries extends DadesEspanya {
 class DadesBalears extends DadesEspanya {
     protected getDadesMínimContribuent() {
         return {base: super.getDadesMínimContribuent().base, major65: 7370, major75: 8450};
+    }
+
+    protected getDadesMínimDescendent() {
+        let estatal = super.getDadesMínimDescendent();
+        estatal.descendents[2] = 4400;
+        estatal.descendents[3] = 4950;
+        return estatal;
+    }
+
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3300, major65: 9900, assistència: 3300};
     }
 }
 
@@ -138,15 +171,33 @@ class DadesGalícia extends DadesEspanya {
     protected getDadesMínimContribuent() {
         return {base: 5789, major65: 6988, major75: 8448};
     }
+
+    protected getDadesMínimDescendent() {
+        return {menor3: 2920, descendents: [2503, 2816, 4172, 4694]};
+    }
+
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3129, major65: 9387, assistència: 3129};
+    }
 }
 
 class DadesLaRioja extends DadesEspanya {
-
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3300, major65: 9900, assistència: 3000};
+    }
 }
 
 class DadesMadrid extends DadesEspanya {
     protected getDadesMínimContribuent() {
         return {base: 5777.55, major65: 6974.70, major75: 8432.10};
+    }
+
+    protected getDadesMínimDescendent() {
+        return {menor3: 2914.80, descendents: [2498.40, 2810.70, 4400, 4950]};
+    }
+
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3123, major65: 9369, assistència: 3123};
     }
 }
 
@@ -157,6 +208,14 @@ class DadesMúrcia extends DadesEspanya {
 class DadesPaísValencià extends DadesEspanya {
     protected getDadesMínimContribuent() {
         return {base: 6105, major65: 7370, major75: 8910};
+    }
+
+    protected getDadesMínimDescendent() {
+        return {menor3: 3080, descendents: [2640, 2970, 4400, 4950]};
+    }
+
+    protected getDadesMínimDiscapacitat() {
+        return {major33: 3300, major65: 9900, assistència: 3300};
     }
 }
 
